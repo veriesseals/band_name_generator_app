ac61bafda695f6fa5b23697e99edbc344e6175b7
Veries Seals <veries@me.com>
2025-03-23 12:09:18 -0500

    Created Varibles and Print Statements for each input and concatinated the names together at the end.

---
+++ band_name_generator_app.py
@@ -0,0 +1,26 @@
+welcome = "Welcome to the Band Name Generator app"
+print(welcome)
+
+
+# Create city_name variable
+# ---------------------------------------
+city_name = input("What city did you grow up in?\n ")
+
+# Print name of city
+# ---------------------------------------
+print(city_name)
+
+# Create pet_name variable
+# ---------------------------------------
+pet_name = input("What is the name of your Pet?\n ")
+
+# Print name of pet
+# ---------------------------------------
+print(pet_name)
+
+# Create a band name varible and concatinate both names:
+# ---------------------------------------
+band_name = (city_name + " " + pet_name)
+your_band_name = ("Your band name is now " + band_name)
+
+print(your_band_name +"!")
